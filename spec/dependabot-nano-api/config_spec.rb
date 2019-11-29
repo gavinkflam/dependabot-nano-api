1b645430e96541206abec916cc78a98b2819437b
Gavin Lam <me@gavin.hk>
2019-11-29 14:25:10 +0800

    Change config name for pip index url

--- spec/dependabot-nano-api/config_spec.rb
+++ spec/dependabot-nano-api/config_spec.rb
@@ -6,12 +6,12 @@ describe 'Config' do
   it 'derives config from environment variables' do
     env = {
       'GITLAB_HOST' => 'gitlab.example.com',
-      'PYPI_BASE_URL' => 'https://pypi.example.com/simple/'
+      'PIP_INDEX_URL' => 'https://pypi.example.com/simple/'
     }
     config = DependabotNanoApi::Config.from_env(env)
 
     expect(config[:gitlab_host]).to be('gitlab.example.com')
-    expect(config[:pypi_base_url]).to be('https://pypi.example.com/simple/')
+    expect(config[:pip_index_url]).to be('https://pypi.example.com/simple/')
   end
 
   it 'applies config' do
@@ -20,7 +20,7 @@ describe 'Config' do
     ).to be('https://pypi.python.org/simple/')
 
     config = {
-      pypi_base_url: 'https://pypi.example.com/simple/'
+      pip_index_url: 'https://pypi.example.com/simple/'
     }
     DependabotNanoApi::Config.apply!(config)
 
--- src/dependabot-nano-api/config.rb
+++ src/dependabot-nano-api/config.rb
@@ -7,19 +7,19 @@ module DependabotNanoApi
   module Config
     DEFAULT = {
       gitlab_host: 'gitlab.com',
-      pypi_base_url: 'https://pypi.org/index'
+      pip_index_url: 'https://pypi.org/index'
     }
 
     def self.from_env(env)
       {
         gitlab_host: env['GITLAB_HOST'] || DEFAULT[:gitlab_host],
-        pypi_base_url: env['PYPI_BASE_URL'] || DEFAULT[:pypi_base_url],
+        pip_index_url: env['PIP_INDEX_URL'] || DEFAULT[:pip_index_url],
       }
     end
 
     def self.apply!(config)
       Dependabot::Python::UpdateChecker::IndexFinder.const_set(
-        'PYPI_BASE_URL', config[:pypi_base_url]
+        'PYPI_BASE_URL', config[:pip_index_url]
       )
     end
   end
